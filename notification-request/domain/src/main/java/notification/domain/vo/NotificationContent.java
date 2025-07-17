739aed796905b19caec96eadc9f9652507b60c95
letitgobaby <jinwoo4687@gmail.com>
2025-07-17 22:14:51 +0900

    chore(domain) : Content, Template 도메인 VO 객체에 Id 필드 삽입

--- notification-request/domain/src/main/java/notification/domain/vo/NotificationContent.java
+++ notification-request/domain/src/main/java/notification/domain/vo/NotificationContent.java
@@ -1,11 +1,35 @@
 package notification.domain.vo;
 
-import notification.definition.annotations.ValueObject;
+import java.io.Serializable;
 
-@ValueObject
-public record NotificationContent(
-        String title,
-        String body,
-        String redirectUrl,
-        String imageUrl) {
+import lombok.Getter;
+
+@Getter
+public class NotificationContent implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String contentId;
+    private final String title;
+    private final String body;
+    private final String redirectUrl;
+    private final String imageUrl;
+
+    public NotificationContent(String contentId, String title, String body, String redirectUrl, String imageUrl) {
+        this.contentId = contentId;
+        this.title = title;
+        this.body = body;
+        this.redirectUrl = redirectUrl;
+        this.imageUrl = imageUrl;
+
+    }
+
+    public NotificationContent(String title, String body, String redirectUrl, String imageUrl) {
+        this.contentId = null; // contentId will be set by the entity
+        this.title = title;
+        this.body = body;
+        this.redirectUrl = redirectUrl;
+        this.imageUrl = imageUrl;
+    }
+
 }
